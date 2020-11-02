81085bb50e720234b819f350d7ba9af054064405
zygisau <zygimantasaugunas@gmail.com>
2020-11-02 13:42:07 +0200

    ADD: Print test output to file.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # perceptron
 Iris data classification with perceptron
+Github link: https://github.com/zygisau/iris-data-perceptron
--- main.py
+++ main.py
@@ -18,38 +18,59 @@ def compare_target_to_prediction(prediction_value, target_value):
     return prediction_class == target_value
 
 
+def handle_significant_error(error, iteration_idx, target, prediction):
+    if round(error[-1], 2) != 0:
+        print(f"Prediction error occured on {iteration_idx} iteration; Target: {target}; prediction: {prediction}; "
+              f"error: {round(error[-1], 2)}")
+
+
+def add_to_output(output, inputs, prediction, target):
+    output.append(f"Inputs: {inputs}, Predicted value: {round(prediction, 2)}, Expected value: {round(target, 2)}")
+
+
+def output_to_file(output):
+    file = open('predictions.txt', 'w')
+    for line in output:
+        file.write(line)
+        file.write('\n')
+    file.close()
+
+
 def test_model(test_data):
     test_data_inputs = [group[:-1] for group in test_data]
     test_data_targets = [group[-1] for group in test_data]
     good_predictions = 0
     error = list()
-    for input_group, target in zip(test_data_inputs, test_data_targets):
+    output = []
+    for idx, (input_group, target) in enumerate(zip(test_data_inputs, test_data_targets)):
         prediction = perceptron.predict(input_group)
         error.append(abs(prediction - target))
+        handle_significant_error(error, idx, target, round(prediction, 2))
         good_predictions += compare_target_to_prediction(prediction, target)
+        add_to_output(output, input_group, prediction, target)
 
-    error_percentage = "{0:.0%}".format(mean(error))
     error_rounded = [round(val, 2) for val in error]
-    print(f"Errors: {error_rounded}")
-    print(f"Mean error: {error_percentage}")
-
     accuracy_percentage = "{0:.0%}".format(good_predictions / len(test_data_targets))
+    output_to_file(output)
+
+    print(f"Errors: {error_rounded}")
+    print(f"Mean error: {round(mean(error), 2)}")
     print(f"Classification accuracy: {accuracy_percentage}")
 
 
 if __name__ == '__main__':
     threshold = 0.5  # only used with sigmoid function
 
-    data_sets = DataSetType.FIRST_SET
+    data_sets = DataSetType.SECOND_SET
     train_data, test_data = FetchingService.fetch_data(data_sets)
     weights = [1.0, 1.0, 1.0, 1.0, 1.0]
 
     activation_function = threshold_function
     max_iterations = 100
-    learning_rate = 0.5
+    learning_rate = 0.8
 
     perceptron = Perceptron(activation_function=activation_function, compare_function=compare_target_to_prediction,
-                            max_iterations=max_iterations, learning_rate=learning_rate, verbose=False)
+                            max_iterations=max_iterations, learning_rate=learning_rate, verbose=True)
     perceptron.train(train_data, weights)
     print(f"Weights: {[ '%.2f' % elem for elem in perceptron.weights ]}")
 
